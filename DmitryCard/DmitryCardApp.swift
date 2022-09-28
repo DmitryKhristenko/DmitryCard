23f53622063fa5a6f1c3caed3037dc8ea096a06e
Dmitry Khristenko <dmitrykhrist@gmail.com>
2022-09-28 18:51:11 +0300

    Initial Commit

---
+++ DmitryCard/DmitryCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DmitryCardApp.swift
+//  DmitryCard
+//
+//  Created by Дмитрий Х on 28.09.22.
+//
+
+import SwiftUI
+
+@main
+struct DmitryCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
